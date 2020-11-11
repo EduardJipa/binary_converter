f7d4fc6ab40dc0e8a9d5523032a7404c49b55345
Cameron Connor <94772@farnborough.ac.uk>
2020-11-11 11:14:38 +0000

    Implemented doDecimaltoBinary. Not yet fully functional.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,6 +3,8 @@ public class main {
 
     public static void main(String[] args) {
         System.out.print(doBinaryToDecimal("00101010"));
+        System.out.println(doDecimalToBinary(49));
+
     }
     public static int doBinaryToDecimal(String input){
         int runningTotal=0; //!Todo make better bc this is slow
@@ -11,4 +13,19 @@ public class main {
         }
 
         return (runningTotal);};
+
+    public static String doDecimalToBinary(int input){
+        int runningTotal = 0;
+        String runningString = ("");
+        int currentWeight;
+        for(int i = 0; i < 8 ;i++){
+            currentWeight = (int) Math.pow(2,(8-i));
+            if(i%currentWeight > 0){
+                runningTotal -= currentWeight;
+                runningString.concat("1");
+            }else{runningString.concat("0");}
+        }
+
+        return  runningString;
+    }
 }
