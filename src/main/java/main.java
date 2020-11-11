2c8f3bdff77a854dab624d40396ac3e50bd9bdc5
Cameron Connor <94772@farnborough.ac.uk>
2020-11-11 10:44:55 +0000

    Completed Binary -> Decimal with Optimisation

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,14 @@
-public class main {
-    public static void main(String[] args) {
 
+public class main {
+
+    public static void main(String[] args) {
+        System.out.print(doBinaryToDecimal("00101010"));
     }
-    String doBinary(){};
+    public static int doBinaryToDecimal(String input){
+        int runningTotal=0; //!Todo make better bc this is slow
+        for (int i = 0; i< input.length();i++){
+            runningTotal += ((input.charAt(i))==("0").toCharArray()[0] ? 0 : (Math.pow(2,i-1)));
+        }
+
+        return (runningTotal);};
 }
